e7952b73d7a2830c445e6df7b168cfb187ce88b4
rhiodam <rhioda.m@gmail.com>
2019-03-12 14:12:55 +0700

    Initial project

---
+++ src/main/java/com/rhiodamuthie/springbootin28m/SpringBootIn28mApplication.java
@@ -0,0 +1,13 @@
+package com.rhiodamuthie.springbootin28m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn28mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn28mApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhiodamuthie/springbootin28m/SpringBootIn28mApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rhiodamuthie.springbootin28m;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootIn28mApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
